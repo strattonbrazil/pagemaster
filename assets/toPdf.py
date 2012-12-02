0de427affe1993480a7444636039e8742b834fc9
Josh Stratton <strattonbrazil@gmail.com>
2012-12-01 17:41:48 -0800

    Added more formatting for options and ending and fixed path issue of images.

--- assets/toPdf.py
+++ assets/toPdf.py
@@ -24,6 +24,19 @@ print("""
 pageTitles = []
 pageRefs = defaultdict(list)
 
+def getGraphic(imgPath):
+    imgPath = os.path.abspath(imgPath)
+    print(imgPath)
+    return '\\includegraphics[width=\\textwidth,height=\\textheight,keepaspectratio]{%s}\n\n' % imgPath
+
+def getHorizontalLine():
+    return '''
+\\vskip\\medskipamount % or other desired dimension
+\\leaders\\vrule width \\textwidth\\vskip0.4pt % or other desired thickness
+\\vskip\\medskipamount % ditto
+\\nointerlineskip
+'''
+
 content = ''
 for page in story['pages']:
     if page['title'] != story['start page']:
@@ -31,18 +44,33 @@ for page in story['pages']:
     pageTitles.append(page['title'])
     content += '\n\\label{%s}\n' % page['title']
 
-    if 'image' in page:
-        content += '\\includegraphics[width=\\textwidth,height=\\textheight,keepaspectratio]{%s}\n\n' % page['image']
+    if 'imageFirst' not in page:
+        page['imageFirst'] = True
+
+    if 'image' in page and page['imageFirst']:
+        content += getGraphic(page['image'])
 
     content += '%s\n\n' % page['content']
     content += '\\vspace{1 em}\n'
     
-    if 'options' in page: # if not story-ender
+    if 'image' in page and not page['imageFirst']:
+        content += getGraphic(page['image'])
+
+    if 'options' in page and page['options']: # if not story-ender
+        content += '\\null\n'
+        content += '\\vfill\n'
+
+        content += getHorizontalLine()
         for option in page['options']:
             choice, dstPage = option
             pageRefs[dstPage].append(page['title'])
-            content += '\\noindent %s, goto page \\pageref{%s}.\n\n' % (choice, dstPage)
+            content += '\\hfill \\textit{%s, goto page \\pageref{%s}.}\n\n' % (choice, dstPage)
             content += '\n'
+    else:
+        content += '''
+\\centerline{ \\bf{ The End } }
+'''
+
 
 for title in pageTitles:
     if title not in pageRefs and title != story['start page']:
@@ -55,6 +83,7 @@ print('\n...done building latex file\n')
 
 latexFile.write(r"""
 \documentclass{article}
+\usepackage[margin=.5in, paperwidth=5in, paperheight=8in]{geometry}
 \usepackage{lipsum}
 \usepackage{graphicx}
 
