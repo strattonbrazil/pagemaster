0602e9e4b36fe1dabe22f045126cb9ed9c22a433
Josh Stratton <strattonbrazil@gmail.com>
2013-07-06 09:27:45 -0700

    Implemented mouse under feature

--- src/pagemaster.py
+++ src/pagemaster.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import collections
 from PyQt4 import QtGui, QtCore, QtWebKit
 
 import signal
@@ -17,20 +18,22 @@ widget.setLayout(layout)
 class Workspace(QtGui.QWidget):
     def __init__(self, *args):
         super(Workspace, self).__init__(*args)
+        self.setMouseTracking(True)
+        self._selectedPage = None
+        self._hoverPage = None
 
         self.story = {
             'title' : '(title of story)',
             'author' : '(your name goes here)',
             'illustrator' : '(leave blank if none)',
             'start page' : '',
-            'pages' : [
-                { 
-                    'title' : 'first page',
-                    'image' : None,
-                    'content' : 'This is the beginning of your story',
-                    'options' : []
-                    }
-                ]
+            'pages' : collections.OrderedDict([
+                ('first page', { 'image' : None,
+                                 'content' : 'This is the beginning of your story',
+                                 'options' : [],
+                                 }
+                 )
+                ])
             }
 
     def paintEvent(self, event):
@@ -38,31 +41,57 @@ class Workspace(QtGui.QWidget):
 
         painter.fillRect(0, 0, self.width(), self.height(), QtCore.Qt.darkGray)
 
-        for page in self.story['pages']:
-            if 'meta' not in page:
-                page['meta'] = {}
-            if 'position' not in page['meta']:
-                page['meta']['position'] = { 'x' : 50, 'y' : 50 }
+        for pageTitle, pageInfo in self.story['pages'].iteritems():
+            if 'meta' not in pageInfo:
+                pageInfo['meta'] = {}
+            if 'position' not in pageInfo['meta']:
+                pageInfo['meta']['position'] = { 'x' : 50, 'y' : 50 }
 
-            position = page['meta']['position']
+            position = pageInfo['meta']['position']
 
             fm = QtGui.QFontMetrics(painter.font())
-            titleWidth = fm.width(page['title'])
+            titleWidth = fm.width(pageTitle)
             titleHeight = fm.height()
 
             margin = 10
 
+            boxWidth = titleWidth+margin*2
+            boxHeight = titleHeight+margin*2
+
+            highlightColor = None
+            if pageTitle == self._hoverPage and pageTitle == self._selectedPage:
+                highlightColor = QtCore.Qt.blue
+            elif pageTitle == self._selectedPage:
+                highlightColor = QtCore.Qt.black
+            elif pageTitle == self._hoverPage:
+                highlightColor = QtCore.Qt.green
+
+            if highlightColor:
+                prehighlightMargin = 6
+                painter.setBrush(highlightColor)
+                painter.drawRoundedRect(position['x']-prehighlightMargin,
+                                        position['y']-prehighlightMargin,
+                                        boxWidth+prehighlightMargin*2,
+                                        boxHeight+prehighlightMargin*2,
+                                        margin, margin)
+
             painter.setBrush(QtCore.Qt.yellow)
             painter.drawRoundedRect(position['x'],
                                     position['y'],
-                                    titleWidth+margin*2,
-                                    titleHeight+margin*2,
+                                    boxWidth,
+                                    boxHeight,
                                     margin, margin)
+
+
+            # save off for picking
+            pageInfo['meta']['boxWidth'] = boxWidth
+            pageInfo['meta']['boxHeight'] = boxHeight
+
 #            painter.fillRect(
 #                             QtCore.Qt.yellow)
             painter.drawText(position['x']+margin,
                              position['y']+titleHeight+margin,
-                             page['title'])
+                             pageTitle)
 
     def sizeHint(self):
         return QtCore.QSize(800, 600)
@@ -82,13 +111,38 @@ class Workspace(QtGui.QWidget):
             self.story['pages']
 
     def addPage(self, x, y):
-        self.story['pages'].append({ 'title' : 'new page',
-                                     'meta' : { 'position' : { 'x' : x, 'y' : y } },
-                                     })
-        self.update()
+        raise NotImplementedError()
+
+#        self.story['pages'].append({ 'title' : 'new page',
+#                                     'meta' : { 'position' : { 'x' : x, 'y' : y } },
+#                                     })
+#        self.update()
 
     def mousePressEvent(self, event):
-        print('something pressed')
+        self._selectedPage = self._pageUnderMouse(event.pos())
+
+        self.update()
+#        print(event.button() )
+#        print('something pressed')
+
+    def mouseMoveEvent(self, event):
+        self._hoverPage = self._pageUnderMouse(event.pos())
+        self.update()
+
+    def _pageUnderMouse(self, pos):
+        for pageTitle,pageInfo in self.story['pages'].iteritems():
+            pagePos = pageInfo['meta']['position']
+
+            pageRect = QtCore.QRect(pageInfo['meta']['position']['x'],
+                                    pageInfo['meta']['position']['y'],
+                                    pageInfo['meta']['boxWidth'],
+                                    pageInfo['meta']['boxHeight'])
+
+            if pageRect.contains(pos):
+                return pageTitle
+
+        return None
+
     
 
 workspace = Workspace()
